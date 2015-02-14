b2473ec780d4edbdc1fc74f9d3d24994a9592e27
Adam <adam@xyzdigital.com>
2015-02-14 10:25:53 -0800

    Add URL to get all messages

--- LOML/urls.py
+++ LOML/urls.py
@@ -4,5 +4,6 @@ from LOML import views
 
 urlpatterns = patterns('',
     url(r'^get_message/$', views.message, name='LOML.views.message'),
+    url(r'^get_messages/$', views.messages, name='LOML.views.messages'),
     url(r'^get_url/$', views.url, name='LOML.views.url'),
 )
--- LOML/views.py
+++ LOML/views.py
@@ -1,3 +1,5 @@
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from datetime import date
@@ -11,6 +13,16 @@ def message(request):
         raise Http404
 
     return HttpResponse(message.text)
+
+def messages(request):
+    try:
+        messages = Message.objects.all().values_list('date', 'text', 'url')
+    except:
+        raise Http404
+
+    messages_json = json.dumps(list(messages), cls=DjangoJSONEncoder)
+
+    return HttpResponse(messages_json, content_type="application/json")
 
 def url(request):
     try:
